22e1f51d14b20416ae9060f8d9cc38d3b92c89a6
nikuz <micurino@gmail.com>
2015-07-03 19:35:32 +0300

    Added planed shutdown timer #

--- server.js
+++ server.js
@@ -13,3 +13,13 @@ require('./routes')(app);
 app.listen(app.PORT, function() {
   console.log('%s: Node server started on %s:%d ...', new Date(Date.now()), app.SERVER, app.PORT);
 });
+
+var getRandomFromRange = function(min, max) {
+  return Math.random() * (max - min) + min;
+};
+
+// exit node every 30-45 minutes. This is going to help with fixing memory leaks
+setTimeout(function() {
+  console.log('Planned shutdown...');
+  process.exit(1);
+}, getRandomFromRange(30, 45) * 60 * 1000);
